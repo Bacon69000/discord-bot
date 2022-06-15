156514813301885c38a190f96d258b27adceae08
Bacon69000 <97658439+Bacon69000@users.noreply.github.com>
2022-06-15 14:14:06 +0700

    welcome messages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const  Discord = require('discord.js')
 require('dotenv').config()
 
+const generateImage = require('./generateImage')
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -17,6 +19,16 @@ client.on('messageCreate', (message) => {
     if (message.content === '!ping') {
         message.reply('Pong!')
     }
+})
+
+const welcomeChannelID = '986505216897720330'
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelID).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
